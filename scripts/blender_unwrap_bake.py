081f83b9d0048ccf63429017896593ac33473b3b
ChrisEurolog <118467027+ChrisEurolog@users.noreply.github.com>
2026-03-24 10:45:17 +0000

    Increase cage extrusion value from 0.03 to 0.04

--- scripts/blender_unwrap_bake.py
+++ scripts/blender_unwrap_bake.py
@@ -174,7 +174,7 @@ def process():
             type='EMIT',
             use_selected_to_active=True,
             use_cage=True,
-            cage_extrusion=0.03,
+            cage_extrusion=0.04,
             margin=8,
             margin_type='EXTEND'
         )
